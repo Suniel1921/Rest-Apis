7a5c6c301db1ee0a4067126d7b33b8269820b31e
sunil <logins697@gmail.com>
2024-04-22 13:07:45 +0545

    add user model

---
+++ src/user/userModel.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import { User } from './userTypes';
+const userSchema = new mongoose.Schema<User>({
+    name : {
+        type : String,
+        required : [true, 'Name is required'],
+    },
+    email : {
+        type : String,
+        required : [true, 'Email is required'],
+        unique : true,
+    },
+    password : {
+        type : String,
+        required : [true, 'Password is required'],
+    }
+},{timestamps: true})
+
+
+export default mongoose.model<User>('User', userSchema);
+
