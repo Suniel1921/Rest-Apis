67908f4477fd93a2e95ae0e9a74da8e565eba2f7
sunil <logins697@gmail.com>
2024-04-22 13:36:51 +0545

    singn jwt token

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
 import bcrypt from 'bcrypt';
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -20,11 +22,15 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 
   //password hash
-  const hashedPassword = bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = await userModel.create({name, email, password: hashedPassword});
+
+  //token genertion jwt
+  const token = sign({sub: newUser._id}, config.jwtSecret as string, {expiresIn: '7d', algorithm: 'HS256'})
 
   //prorcess
   //response
-  res.json({ message: "user created" });
+  res.json({ accessToken: token});
 };
 
 export { createUser };
