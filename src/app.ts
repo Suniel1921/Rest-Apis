963856ea04a76a2fb560d4c2ca14b754616bbe9d
sunil <logins697@gmail.com>
2024-04-22 12:42:15 +0545

    create user router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import createHttpError from "http-errors";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
@@ -10,6 +11,8 @@ const app = express();
 app.get("/", (req, res, next) => {
   res.json({ message: "welcome to the ebook api" });
 });
+
+app.use('/api/users',userRouter)
 
 //Global error handler --- make sure global erro handler sab se last me ho
 app.use(globalErrorHandler);
---
+++ src/user/userRouter.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const userRouter = express.Router();
+import { createUser } from './userController';
+
+//routes
+userRouter.post("/register",createUser)
+
+
+
+
+export default userRouter;
