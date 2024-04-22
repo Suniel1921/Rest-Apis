40fb3900b263ca841aff7b9a4abe14ce0f449a01
sunil <logins697@gmail.com>
2024-04-22 11:24:36 +0545

    add config

--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import app from './src/app';
+import { config } from './src/config/config';
 
 const startServer = ()=>{
        try {
-              const port = process.env.PORT || 3000;
+              // const port = process.env.PORT || 3000;
+              const port = config.port || 3000;
 
               app.listen(port, ()=>{
                      console.log(`Server is running on port no : ${port}`);
